567950692fa7b9eff7c47231a55d168083a4b95d
patrickmccauley <113490012+patrickmccauley@users.noreply.github.com>
2022-12-05 20:00:21 -0600

    Update Day5.py
    can now compute part 1 and 2

--- Day5.py
+++ Day5.py
@@ -6,7 +6,7 @@ with open('input.txt', "r") as f:
 f.close()
 
 #create stacks
-stack = [["N", "B", "D", "T", "V", "G", "Z", 'j'], 
+stack = [["N", "B", "D", "T", "V", "G", "Z", 'J'], 
          ["S", "R", "M", "D", "W", "P", "F"], 
          ['V', 'C', 'R', 'S', 'Z'], 
          ['R', 'T','J','Z','P','H','G'],
@@ -21,6 +21,7 @@ stack = [["N", "B", "D", "T", "V", "G", "Z", 'j'],
 # Format data to integers.
 data = data.splitlines()
 
+
 ##move_crate functions
 #x = quantity  
 #y = origin 
@@ -33,23 +34,39 @@ def move_crate(x,y,z):
   
   return stack
 
+def move_multiple(x,y,z):
+    tempstack = []
+    for u in range(x):
+        
+        tempstack.append(stack[y-1].pop())
+    
+    for p in range(0,len(tempstack)):
+        stack[z-1].append(tempstack.pop())
+    
+    return stack
+    
+#feeds parameters through 
 for x in range(0, len(data)-2):
     line = data[x]
     q = int(line[0:line.index(",")])
     y  = int(line[line.index(",") + 1: line.index(".")])
     z = int(line[line.index(".")+1])
-
-    move_crate(q,y,z)
-    print(stack)
-    
-#print final output
-for j in range (0, 9):
-     print(stack[j].pop())
-
     
     
+    #move_crate(q,y,z) #uncomment for part 1
+    #move_multiple(q,y,z) #uncomment for part  2
+    
+#print final output\
+print
+print("final stack")
+print(stack)
+print
+for x in stack:
+    print (x[-1])
 
-
+# if final output fails find the last letter of each stack manually
+# for j in range (0, 9):
+#     print(stack[j].pop())
 
 
 
